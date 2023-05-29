756c351bf448840db8d5593a8355010b9c753cfe
Geoffrey Hazard <Geoffrey_Hazard@brown.edu>
2023-05-29 07:09:38 -0700

    test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> My Portfolio Site!
+          My Portfolio Site! Geoffrey Hazard is a designer.
         </p>
         <a
           className="App-link"
